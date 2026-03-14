4d5d08eb73af35301a2053ea44017737961c1f23
gkhushi21 <152429143+gkhushi21@users.noreply.github.com>
2026-03-14 15:22:26 +0530

    Add files via upload

---
+++ Assessment_14_03_26/src/main/java/com/cg/bean/Librarian.java
@@ -0,0 +1,39 @@
+package com.cg.bean;
+
+public class Librarian {
+	private int librarianId;
+	private String name;
+	private Book book;
+	public Librarian() {
+		
+	}
+	public int getLibrarianId() {
+		return librarianId;
+	}
+	public void setLibrarianId(int librarianId) {
+		this.librarianId = librarianId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	
+	public Librarian(int librarianId, String name, Book book) {
+		this.librarianId = librarianId;
+		this.name = name;
+		this.book = book;
+	}
+	public void issueBook() {
+		System.out.println("Librarian Name:"+name);
+		getBook().displayBookDetails();
+	}
+
+}
